444a490859043ba9344295e80b83f3818a447b71
Fabian A Ortiz Chavez <alonsofabianortiz@icloud.com>
2025-02-02 23:48:05 -0700

    arrow lab

---
+++ index.js
@@ -0,0 +1,18 @@
+function divide() {
+    return 2000 / 100; 
+}
+
+const square = (parameter3, parqameter3) => {
+console.log(`multiply ${parameter3}`);
+console.log(`multiply ${parameter3}`);
+return parameter3 * parameter3;
+}
+square(3,3)
+
+const add = (num10, num5) => {
+    
+    console.log(`adding ${num10}`);
+    console.log(`adding ${num5}`);
+    return num10 + num5;
+}
+add(10, 5)
